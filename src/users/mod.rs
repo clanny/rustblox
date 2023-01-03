1c9477efeb2246a794077b85e6aad13de71267d1
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-01-03 22:30:13 +0100

    Move user types

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -2,22 +2,3 @@ use serde::{Deserialize, Serialize};
 
 pub mod display_names;
 pub mod users;
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct User {
-    pub description: String,
-    pub created: String,
-    pub isBanned: bool,
-    pub externalAppDisplayName: Option<String>,
-    pub hasVerifiedBadge: bool,
-    pub id: usize,
-    pub name: String,
-    pub displayName: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct PartialUser {
-    pub id: usize,
-    pub name: String,
-    pub displayName: String,
-}
--- src/users/users.rs
+++ src/users/users.rs
@@ -1,17 +1,40 @@
-use crate::{
-    users::{PartialUser, User},
-    util::jar::RequestJar,
-    util::Error,
-};
+use serde::{Deserialize, Serialize};
 
+use crate::{util::jar::RequestJar, util::Error};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub description: String,
+    pub created: String,
+    pub is_banned: bool,
+    pub external_app_display_name: Option<String>,
+    pub has_verified_badge: bool,
+    pub id: usize,
+    pub name: String,
+    pub display_name: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct MinimalAuthenticatedUser {
+    pub id: usize,
+    pub name: String,
+    pub display_name: String,
+}
+
+/// Gets a user by their user ID
+///
+/// # Error codes
+/// 3: The user id is invalid
 pub async fn user_by_id(jar: &mut RequestJar, user_id: usize) -> Result<User, Box<Error>> {
     let url = format!("https://users.roblox.com/v1/users/{}", user_id);
     let response = jar.get_json::<User>(&url).await?;
     Ok(response)
 }
 
-pub async fn whoami(jar: &mut RequestJar) -> Result<PartialUser, Box<Error>> {
+pub async fn whoami(jar: &mut RequestJar) -> Result<MinimalAuthenticatedUser, Box<Error>> {
     let url = "https://users.roblox.com/v1/users/authenticated";
-    let response = jar.get_json::<PartialUser>(&url).await?;
+    let response = jar.get_json::<MinimalAuthenticatedUser>(&url).await?;
     Ok(response)
 }
