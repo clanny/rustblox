85f4531f0970fc60c1d784df5e8b4fe3be16959a
piano1029 <46605242+piano1029@users.noreply.github.com>
2023-01-03 22:29:32 +0100

    Add more error types

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -8,6 +8,7 @@ pub enum Error {
     Network,
     JSON,
     Authentication,
+    Throttled,
 }
 
 fn error_to_user_message(err: Error) -> String {
@@ -15,5 +16,6 @@ fn error_to_user_message(err: Error) -> String {
         Error::Network => "Network error".to_string(),
         Error::JSON => "JSON error".to_string(),
         Error::Authentication => "Authentication error".to_string(),
+        Error::Throttled => "Throttled, too many requests".to_string(),
     }
 }
--- src/util/status_codes.rs
+++ src/util/status_codes.rs
@@ -5,6 +5,8 @@ pub fn status_code_to_error(status_code: StatusCode) -> Option<Error> {
     match status_code {
         StatusCode::OK => None,
         StatusCode::UNAUTHORIZED => Some(Error::Authentication),
+        StatusCode::FORBIDDEN => Some(Error::Authentication),
+        StatusCode::TOO_MANY_REQUESTS => Some(Error::Throttled),
         _ => Some(Error::Network),
     }
 }
